5a8cddd680bc350fdf3e93930e1a35c4a4a9cf9a
Dan Prince <dan.prince@rackspace.com>
2010-09-21 22:57:02 -0400

    Append a unique HTTP URL parameter to the list servers request to avoid cached responses. Added test case on the Connection.list_servers method.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,19 @@ $:.unshift File.dirname(__FILE__) + '/../lib'
 require 'cloudservers'
 require 'rubygems'
 require 'mocha'
+
+module TestConnection
+
+def get_test_connection 
+
+    conn_response = {'x-server-management-url' => 'http://server-manage.example.com/path', 'x-auth-token' => 'dummy_token'}
+    conn_response.stubs(:code).returns('204')
+    server = mock(:use_ssl= => true, :verify_mode= => true, :start => true, :finish => true)
+    server.stubs(:get).returns(conn_response)
+    Net::HTTP.stubs(:new).returns(server)
+
+    CloudServers::Connection.new(:username => "test_account", :api_key => "AABBCCDD11")
+
+end
+
+end
